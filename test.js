7f6b9ab07929d05d9761cca817aa920c7f075ed4
ragingwind <ragingwind@gmail.com>
2015-07-29 15:48:50 +0900

    Add more push test

--- test.js
+++ test.js
@@ -35,3 +35,21 @@ it('should returns prev items', function () {
 	assert.strictEqual(itr.prev(), 1);
 	assert.strictEqual(itr.prev(), 0);
 });
+
+it('should returns pushed items', function () {
+	var itr = new Iterarry();
+	itr.push(0, 1, 2, 3, 4, 5);
+	
+	assert.strictEqual(itr.next(), 0);
+	assert.strictEqual(itr.next(), 1);
+	assert.strictEqual(itr.next(), 2);
+	assert.strictEqual(itr.next(), 3);
+	assert.strictEqual(itr.next(), 4);
+	assert.strictEqual(itr.next(), 5);
+	assert.strictEqual(itr.next(), 0);
+	assert.strictEqual(itr.next(), 1);
+	assert.strictEqual(itr.next(), 2);
+	assert.strictEqual(itr.next(), 3);
+	assert.strictEqual(itr.next(), 4);
+	assert.strictEqual(itr.next(), 5);
+});
